6d6b43d39b4376220054663adaa5d08846969020
Hyungchul You <teoyou881@gmail.com>
2023-12-09 11:21:43 -0500

    feat(_component): create a private folder named _component

---
+++ src/app/(beforeLogin)/@modal/(.)i/flow/signup/page.tsx
@@ -0,0 +1,9 @@
+import styles from '@/app/(beforeLogin)/_component/Main.module.css';
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      sign up modal
+    </main>
+  );
+}
