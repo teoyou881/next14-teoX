39df323acefaed9c42e9f28b18e0134a82a42d09
Hyungchul You <teoyou881@gmail.com>
2023-12-28 02:22:25 -0500

    feat(rightSearchZone): create component and css

---
+++ src/app/(afterLogin)/_component/RightSearchZone.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import style from './rightSearchZone.module.css';
+import React from 'react';
+import { usePathname } from 'next/navigation';
+import SearchForm from '@/app/(afterLogin)/_component/SearchForm';
+
+export default function RightSearchZone() {
+  let pathname = usePathname();
+  const onChangeFollow = () => {};
+  const onChangeAll = () => {};
+  if (pathname === '/explore') return null;
+  if (pathname === '/search') {
+    return (
+      <div>
+        <h5 className={style.filterTitle}>Search filtersx</h5>
+        <div className={style.filterSection}>
+          <div>
+            <label>People</label>
+            <div className={style.radio}>
+              <div>From anyone</div>
+              <input type='radio' name='pf' defaultChecked onChange={onChangeAll} />
+            </div>
+            <div className={style.radio}>
+              <div>People you follow</div>
+              <input type='radio' name='pf' value='on' onChange={onChangeFollow} />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  return (
+    <div style={{ marginBottom: 60, width: 'inherit' }}>
+      <SearchForm />
+    </div>
+  );
+}
