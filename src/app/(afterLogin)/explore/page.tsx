83be1f042c0e274b96f2529755c042510e0e9a6b
Hyungchul You <teoyou881@gmail.com>
2023-12-28 02:23:58 -0500

    feat(explore): create component and css

--- src/app/(afterLogin)/explore/page.tsx
+++ src/app/(afterLogin)/explore/page.tsx
@@ -1,7 +1,23 @@
-export default function Page(){
-    return (
-        <main>
-            <h1>explore</h1>
-        </main>
-    )
+import SearchForm from '@/app/(afterLogin)/_component/SearchForm';
+import React from 'react';
+import Trend from '@/app/(afterLogin)/_component/Trend';
+import style from './explore.module.css';
+
+export default function Page() {
+  return (
+    <main className={style.main}>
+      <div className={style.formZone}>
+        <SearchForm />
+      </div>
+      <div className={style.trend}>
+        <h3>For you</h3>
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+      </div>
+    </main>
+  );
 }
