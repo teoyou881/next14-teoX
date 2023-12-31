0afc886cf65b06098aae5dedd668f57fb4cbc452
Hyungchul You <teoyou881@gmail.com>
2023-12-30 23:59:17 -0500

    feat(search): create component and css

--- src/app/(afterLogin)/search/page.tsx
+++ src/app/(afterLogin)/search/page.tsx
@@ -1,7 +1,40 @@
-export default function Page() {
-    return (
-        <main>
-            <h1>search</h1>
-        </main>
-    )
+import style from './search.module.css';
+import Post from '@/app/(afterLogin)/_component/Post';
+import Tab from '@/app/(afterLogin)/home/_component/Tab';
+import SearchForm from '@/app/(afterLogin)/_component/SearchForm';
+import BackButton from '@/app/(afterLogin)/_component/BackButton';
+
+type Props = {
+  searchParams: { q: string, f?: string, pf?: string };
+}
+export default function Search({ searchParams }: Props) {
+  return (
+    <main className={style.main}>
+      <div className={style.searchTop}>
+        <div className={style.searchZone}>
+          <div className={style.buttonZone}>
+            <BackButton />
+          </div>
+          <div className={style.formZone}>
+            <SearchForm q={searchParams.q} />
+          </div>
+        </div>
+        <Tab />
+      </div>
+      <div className={style.list}>
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        {/*<SearchResult searchParams={searchParams} />*/}
+      </div>
+    </main>
+  );
 }
